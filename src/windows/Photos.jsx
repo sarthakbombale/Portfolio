43a153811bbed5ab5fae4a01a0c6a6b00d722426
sarthakbombale <sarthakbomble250@gmail.com>
2026-04-13 00:18:36 +0530

    photos page updated

--- src/windows/Photos.jsx
+++ src/windows/Photos.jsx
@@ -10,13 +10,13 @@ const Photos = () => {
         <h2 className="flex-1 text-center font-bold text-sm">Photos</h2>
       </div>
 
-      <div id="photos" className="flex h-[500px] bg-white">
+      <div className="flex h-125 bg-white">
         {/* Sidebar - Matches your #photos .sidebar CSS */}
         <div className="sidebar">
           <h2>Library</h2>
           <ul>
             <li>
-              <img src="/icons/photos.png" alt="All Photos" className="w-4" />
+              <img src="/images/photos.png" alt="All Photos" className="w-4" />
               <p>All Photos</p>
             </li>
             <li className="opacity-50">
@@ -36,7 +36,6 @@ const Photos = () => {
             <li><img src="/images/project-2.png" alt="Gallery 2" /></li>
             <li><img src="/images/project-3.png" alt="Gallery 3" /></li>
             <li><img src="/images/project-4.png" alt="Gallery 4" /></li>
-            <li><img src="/images/project-5.png" alt="Gallery 5" /></li>
           </ul>
         </div>
       </div>
